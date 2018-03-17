433601a3981e133ceb99b607ce563e49725dfa55
ExcelDev <37254457+ExcelDev@users.noreply.github.com>
2018-03-17 08:07:02 +0800

    Add files via upload

---
+++ src/pow_control.h
@@ -0,0 +1,8 @@
+#ifndef POW_CONTROL_H
+#define POW_CONTROL_H
+
+extern bool fTestNet;
+static const int LAST_POW_BLOCK = 2103840; //after this block Excel Coin will turn to fully PoS Coin
+
+
+#endif // POW_CONTROL_H
